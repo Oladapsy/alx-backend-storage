15bbca7b9a6b287edbed6e0fcb6bb77613f39bac
oladapsy <dapoodedeyi@yahoo.com>
2024-10-18 13:14:01 +0000

    restest

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,6 @@
 -- a SQL script that creates a table users
-
 CREATE TABLE users IF NOT EXISTS (
-	id INT NOT_NULL AUTO_INCREMENT PRIMARY KEY,
-	email CHAR(255) NOT_NULL UNIQUE,
-       	name CHAR(255),
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email CHAR(255) NOT_NULL UNIQUE,
+    name CHAR(255),
 );
