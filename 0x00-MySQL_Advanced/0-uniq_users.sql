fdc9b13e929917564a113db8998612efb4693c98
oladapsy <dapoodedeyi@yahoo.com>
2024-10-18 13:18:10 +0000

    testme

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
 -- a SQL script that creates a table users
-CREATE TABLE users IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email CHAR(255) NOT_NULL UNIQUE,
     name CHAR(255),
