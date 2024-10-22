abdd50cf980e19a9eb7a0bef90c6f8897ad8b399
oladapsy <dapoodedeyi@yahoo.com>
2024-10-22 07:10:26 -0700

    testit

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         logs stored in MongoDB:
     """
 
-    client = MongoClient(host="localhost", port=27017)
+    client = MongoClient("mongodb://localhost:27017")
     db = client.logs
     nginx_collection = db.nginx
 
