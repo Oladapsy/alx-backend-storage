6b62345415f30e5c5258214be5d9b40cd2dcd2ac
oladapsy <dapoodedeyi@yahoo.com>
2024-10-22 04:47:25 -0700

    test

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -2,10 +2,11 @@
 """a Python function that lists all documents in a collection:"""
 from pymongo import MongoClient
 
+
 def list_all(mongo_collection):
     """list all document in a collection"""
-   documents = mongo_collection.find()
+    documents = mongo_collection.find()
 
-   if documents.count() == 0:
-       return []
-   return documents
+    if documents.count() == 0:
+        return []
+    return documents
