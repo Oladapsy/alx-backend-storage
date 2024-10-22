142b3e68f0c9d66c36694de81e5c8acfc5c131c7
oladapsy <dapoodedeyi@yahoo.com>
2024-10-22 04:49:19 -0700

    test2

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -7,6 +7,6 @@ def list_all(mongo_collection):
     """list all document in a collection"""
     documents = mongo_collection.find()
 
-    if len(documents) == 0:
+    if documents.count() == 0:
         return []
     return documents
